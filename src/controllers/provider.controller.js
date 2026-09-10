a890770cc0660366b51d24dee3a3dde7db0b0524
Bigdanny27 <danieloriakhi27@gmail.com>
2026-09-10 16:33:59 +0100

    init

--- src/controllers/provider.controller.js
+++ src/controllers/provider.controller.js
@@ -1,3 +1,4 @@
+import { ca } from "zod/locales"
 import Provider from "../models/provider.model.js"
 import User from "../models/user.model.js"
 
@@ -33,5 +34,83 @@ export const createProvider = async (req, res) => {
             message: "Internal server error",
             error: error.message
         })
+    }
+}
+
+export const getProviderProfile = async (req, res) => {
+    try {
+        const userId = req.user._id;
+
+        const provider = await Provider.findOne({ user: userId }).populate("user", "name lastname email");
+        if (!provider) {
+            return res.status(404).json({
+                message: "Provider profile not found"
+            });
+        }
+        return res.status(200).json({
+            message: "Provider profile retrieved successfully",
+            provider
+        }); 
+    } catch (error) {
+        return res.status(500).json({
+            message: "Failed to retrieve provider profile",
+            error: error.message
+        });
+    }
+}
+
+export const updateProviderProfile = async (req, res) => {
+    try {
+        const userId = req.user._id;
+        const updatedData = req.body;
+
+        const updatedProvider = await Provider.findOneAndUpdate({ user: userId }, updatedData, { new: true });
+        if (!updatedProvider) {
+            return res.status(404).json({
+                message: "Provider profile not found"
+            });
+        }
+        return res.status(200).json({
+            message: "Provider profile updated successfully",
+            provider: updatedProvider
+        });
+    } catch (error) {
+        return res.status(500).json({
+            message: "Failed to update provider profile",
+            error: error.message
+        });
+    }
+}
+
+export const setAvailability = async (req, res) => {
+    try {
+        const userId = req.user._id;
+        const { isAvailable } = req.body;
+
+        if (typeof isAvailable !== 'boolean') {
+            return res.status(400).json({ message: "isAvailable must be a boolean" });
+        }
+
+        const provider = await Provider.findOneAndUpdate(
+            { user: userId },
+            { isAvailable },
+            { new: true }
+        );
+
+        if (!provider) {
+            return res.status(404).json({
+                message: "Provider profile not found"
+            });
+        }
+
+        return res.status(200).json({
+            message: "Availability status updated successfully",
+            provider
+        });
+    } catch (error) {
+        return res.status(500).json({
+            message: "Failed to update availability status",
+            error: error.message
+        });
     }
 }
